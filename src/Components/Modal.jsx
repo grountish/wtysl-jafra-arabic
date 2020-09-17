70974f44622088bdc23ad412bdffdc5672361b35
MilttO0nMss <grountish@gmail.com>
2020-09-17 14:54:13 +0200

    share links fixed

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -61,10 +61,10 @@ export default class ExampleApp extends React.Component {
                 <div onClick={this.handleCloseModal} className="modal-container">
                 <div className="closeButton" onClick={this.handleCloseModal}><svg stroke="white" fill="white" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="white" stroke="white" stroke-width="2" d="M3,3 L21,21 M3,21 L21,3"></path></svg></div>
                     <h1 style={{textAlign:'center',color:'white'}}>شارك</h1>
-                    <Twitter link="https://whattookyousolong.com/experience" />
-                    <Facebook link="https://whattookyousolong.com/experience" />
-                    <Whatsapp link="https://whattookyousolong.com/experience" />
-                    <Linkedin link="https://whattookyousolong.com/experience" />
+                    <Twitter link="https://whattookyousolong.org/interactive" />
+                    <Facebook link="https://whattookyousolong.org/interactive" />
+                    <Whatsapp link="https://whattookyousolong.org/interactive" />
+                    <Linkedin link="https://whattookyousolong.org/interactive" />
                 </div>
                 </ReactModal>
             </div>
